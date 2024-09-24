5b9e79285428d3e64bb5b8151be3bd2bd9caaa1b
Hanoch Mordkovich <hanoch.mordkovich@techsee.me>
2024-09-24 10:55:54 +0300

    compute-checksum

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "TechSeeLiveFramework", url: "https://sdk-us.techsee.me/ios/build/TechSeeLiveFramework_5.4.4.xcframework.zip", checksum: "9827b532ba8823a82578651f7ca9a5601db5316a77bc7fa3a57563fdbc6bf2ae"),
+        .binaryTarget(name: "TechSeeLiveFramework", url: "https://sdk-us.techsee.me/ios/build/TechSeeLiveFramework_5.4.4.xcframework.zip", checksum: "fc41d755eb2812887048dc459f42cf8319c1a5e9ca9483deef554e4fc92be7d6"),
         
         .binaryTarget(name: "TSWebRTC", url: "https://sdk-us.techsee.me/ios/build/TSWebRTC_1.0.0.xcframework.zip", checksum: "4aa64a8738e95e566eed14a03c1c186b997be7f5a289712e05c28c3f05eefbbc")
     ]
